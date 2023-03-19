2515ae28f026dca1fba1f0488a04f99b78dd368d
Matheus de Oliveira <32457879+matheus-oliveira-andrade@users.noreply.github.com>
2023-03-19 12:56:51 -0300

    feature: transactions seed (#1)
    * feat: add projects layers
    
    * feat: add unit tests projects layers
    
    * chore: remove implicits usings and nullable in csprojs
    
    * feat: transaction seed app
    
    * feat: docker compose

---
+++ transactions-seed-app/src/Seed.Infrastructure/Bus/RabbitMqBusService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using Seed.Domain.Interfaces.Bus;
+using Seed.Infrastructure.Bus.Interfaces;
+
+namespace Seed.Infrastructure.Bus
+{
+    public class RabbitMqBusService : IBusService
+    {
+        private readonly IRabbitMqConnection _rabbitMqConnection;
+
+        public RabbitMqBusService(IRabbitMqConnection rabbitMqConnection)
+        {
+            _rabbitMqConnection = rabbitMqConnection;
+        }
+
+        public void PublishBatch(string exchangeName, string key, List<string> messages)
+        {
+            using var connection = _rabbitMqConnection.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, true);
+
+            var batch = channel.CreateBasicPublishBatch();
+
+            foreach (var message in messages)
+            {
+                var json = JsonSerializer.Serialize(message);
+                var bytes = Encoding.UTF8.GetBytes(json);
+
+                batch.Add(exchangeName, key, false, null, new ReadOnlyMemory<byte>(bytes));
+            }
+
+            batch.Publish();
+        }
+    }
+}
