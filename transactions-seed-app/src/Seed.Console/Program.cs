edc6bf0c2b874ab964bbe82fa131d9ba9aa9ca82
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2023-03-19 12:55:20 -0300

    feat: transaction seed app

---
+++ transactions-seed-app/src/Seed.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Seed.Application;
+using Seed.Infrastructure;
+using Serilog;
+
+namespace Seed.Console
+{
+    [ExcludeFromCodeCoverage]
+    public static class Program
+    {
+        private static IHost BuildHost(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureServices((ctx, services) =>
+            {
+                services.AddHostedService<PublishTransactionsHostedService>();
+                
+                services.AddApplication();
+                services.AddInfrastructure(ctx.Configuration);
+            })
+            .UseSerilog((_, _, loggerConfiguration) => loggerConfiguration
+                .Enrich.FromLogContext()
+                .WriteTo.Console())
+            .Build();
+
+        public static int Main(string[] args)
+        {
+            BuildHost(args).Run();
+            return 0;
+        }
+    }
+}
---
+++ transactions-seed-app/tests/unit-tests/Seed.Infrastructure.Tests/Bus/RabbitMqBusServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Moq;
+using RabbitMQ.Client;
+using Seed.Domain.Interfaces.Bus;
+using Seed.Infrastructure.Bus;
+using Seed.Infrastructure.Bus.Interfaces;
+using Xunit;
+
+namespace Seed.Infrastructure.Tests.Bus
+{
+    public class RabbitMqBusServiceTests
+    {
+        private readonly Mock<IRabbitMqConnection> _rabbitMqConnectionMock;
+        
+        private readonly IBusService _busService;
+
+        public RabbitMqBusServiceTests()
+        {
+            _rabbitMqConnectionMock = new Mock<IRabbitMqConnection>();
+            
+            _busService = new RabbitMqBusService(_rabbitMqConnectionMock.Object);
+        }
+
+
+        [Fact]
+        public void PublicBatch_ShouldDeclareExchangeAndPublishBatch_WithSuccess()
+        {
+            // Arrange
+            var connectionMock = new Mock<IConnection>(); 
+            var modelMock = new Mock<IModel>();
+            var batchMock = new Mock<IBasicPublishBatch>();
+
+            connectionMock
+                .Setup(x => x.CreateModel())
+                .Returns(modelMock.Object);
+
+            modelMock
+                .Setup(x => x.CreateBasicPublishBatch())
+                .Returns(batchMock.Object);
+            
+            _rabbitMqConnectionMock
+                .Setup(x => x.CreateConnection())
+                .Returns(connectionMock.Object);
+
+            // Act
+            _busService.PublishBatch("exchange", "key", new List<string> { "message1", "message2"});
+
+            // Assert
+            modelMock.Verify(x => 
+                x.ExchangeDeclare("exchange", ExchangeType.Fanout, true, false, null));
+
+            batchMock.Verify(x => x.Publish());
+        }
+    }
+}
